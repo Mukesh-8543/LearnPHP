423c0003831b03b3bef3094ef87d9a973670d678
mukesh kumar yadav <my6386887@gmail.com>
2025-01-18 04:08:02 -0930

    New Add Variables

---
+++ Variables.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h2{
+            color:red;
+        }
+        p{
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    <?php 
+        $name = "Mukesh Kumar<br>";
+        $num = 8543;
+        echo "<h2>" . $name . "</h2>";
+        echo "<p>"."Hello How are you : ". $name."</p>";
+        echo $num;
+    ?>
+</body>
+</html>
