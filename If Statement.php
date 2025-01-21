44746e04e58cfd38edb2b5991651113f7d766bb6
mukesh kumar yadav <my6386887@gmail.com>
2025-01-21 03:52:39 -0930

    if Statement Name..

---
+++ If Statement.php
@@ -0,0 +1,23 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=\, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+        $x = 15;
+        $y = 10;
+
+        if($x > $y){
+            echo "X is Greater";
+        }
+        else{
+            echo "X is not Greater..";
+        }
+
+
+?>
+    </body>
+    </html>
