e3de39242b4f104c11fcd5cb2a56ba708071c2e3
mukesh kumar yadav <my6386887@gmail.com>
2025-01-21 01:53:44 -0930

    Data Type Name

---
+++ datatype.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php
+                // (String)
+                $x = "Mukesh Kumar";
+                //(intger)
+                $x = 2500;
+                //(float)
+                $x = 25.50;
+                //(boolen)
+                $x = true;
+                //(array)
+                $x = array("HTML","CSS","JAVA Script");
+
+                echo $x . "<br>";
+
+                var_dump($x);
+
+        ?>
+</body>
+</html>
