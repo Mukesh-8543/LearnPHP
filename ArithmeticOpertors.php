72d5017c383a4902914c0af38df14941f867d573
mukesh kumar yadav <my6386887@gmail.com>
2025-01-21 02:57:29 -0930

    Arthmetic Opretors Name..

---
+++ ArithmeticOpertors.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <?php
+        $a = 10;
+        $b = 3;
+
+        //$a = $a + $b;
+        $a += $b;
+
+        //$c = $a + $b;
+        //$c = $a ** $b
+      //  $c = ( $a+ $b) * 2;
+    //echo $c;
+    
+    // a = a+1;(incriment...)  a = a-1(dencriment...)
+  //$a++;
+//echo$a;
+
+        
+    echo $a;
+?>
+</body>
+</html>
