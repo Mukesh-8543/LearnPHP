df7ee55d8285cde8d3867d35aca7824d3c9cfaf6
mukesh kumar yadav <my6386887@gmail.com>
2025-01-21 03:31:56 -0930

    Comparisan Operators Name..

---
+++ Comparisan-Operators -name.php
@@ -0,0 +1,39 @@
+                    <!-- Comparison Operators...
+            ==        Equal To($x == $y)
+            ===      Equal valus and equal type
+            !=       not equal
+            <>      not equal
+            !==     not equal value or not aqual type
+            >       Garter than 
+            <       lesh tyhan 
+            >=      Grater than or equla to
+            <=      lesh than or equal to
+
+                    valus true hoga to 1 aayega nahi fales rahega to kucha nahi aayega ....
+             -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+<?php
+       $a = 10;
+       $b = 1;
+
+       //(value awer datatype dono braber ho)
+      //echo $a === $b;
+
+     // (value awer data type dono braber na ho)
+      //  echo $a !== $b;
+       
+       //echo $a > $b;
+       echo $a < $b;
+
+?>
+        
+</body>
+</html>
