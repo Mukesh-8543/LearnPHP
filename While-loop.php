3e521d76501e367cc6abbb7d0521e9d98e64c0ac
mukesh kumar yadav <my6386887@gmail.com>
2025-01-24 02:53:22 -0930

    while loop

---
+++ While-loop.php
@@ -0,0 +1,39 @@
+<?php
+     $y = 1;
+
+     while($y <= 50 ){
+         echo $y . ",";
+     $y = $y+2;
+     }
+ 
+     $z = 1;
+ while($z <= 30){
+     echo $z . ",";
+ $z++;
+ 
+ }
+    
+
+ 
+    
+  $x = 10;
+    while($x >= 1){
+        echo  $x . ")Hello Mukesh Kumar <br>";
+    $x = $x-1;
+
+    }
+
+
+
+    $m = 1;
+
+while($m <= 5){
+    echo $m . ",";
+
+$m =$m+1;
+
+    
+}
+
+?>
+
