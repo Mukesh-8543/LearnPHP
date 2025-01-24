ce58fba51d30f7b05963366507499574ee1b9b22
mukesh kumar yadav <my6386887@gmail.com>
2025-01-24 01:48:02 -0930

    String Opretater

---
+++ String Operator.php
@@ -0,0 +1,12 @@
+<?php
+        $a = "Hello :";
+        $a .= " Mukesh Kumar Yadav <br><br>";
+
+    echo $a;
+
+        $a = "Hello : ";
+
+        $x = $a . "Mo : " .  8543804393;
+
+        echo $x;
+?>
