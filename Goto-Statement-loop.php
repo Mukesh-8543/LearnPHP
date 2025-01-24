1b2e8830be519e803bc27393319248d45a715d5e
mukesh kumar yadav <my6386887@gmail.com>
2025-01-24 03:55:43 -0930

    goto statment loop

---
+++ Goto-Statement-loop.php
@@ -0,0 +1,16 @@
+<?php 
+ for($a = 1; $a <=10; $a++){
+    if($a == 4){
+        goto abc;
+    }
+    echo $a .") Mukesh Kumar <br>";
+} 
+    echo "Hello";
+    abc:
+    echo "Hey this is label ABC."
+
+ 
+
+
+
+?>
