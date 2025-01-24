1b3e570459614f95c89e0090f3a2f00dde1d695b
mukesh kumar yadav <my6386887@gmail.com>
2025-01-24 03:12:06 -0930

    For loop

---
+++ For-loop.php
@@ -0,0 +1,12 @@
+<?php
+         for($a = 1; $a <= 10; $a =$a+1){
+                echo  $a . ") Hello Mukesh Kumar Yadav<br>";
+
+         }
+
+        for($x = 10; $x >= 1; $x = $x-1){
+            echo $x . "*";
+
+        }
+
+?>
