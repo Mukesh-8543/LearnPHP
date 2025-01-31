3de905c4ba43cf736941848b28ce947c44c1515b
mukesh kumar yadav <my6386887@gmail.com>
2025-01-31 04:49:00 -0930

    Array Function second

--- Array-Function.php
+++ Array-Function.php
@@ -151,8 +151,7 @@
         
 
       // Multidimensional Array for loop
-
-             echo "<br><br>";
+      echo   "<h3> Multidimensinaol Array</h3>";
              $emp = [
                 [1,"Anita" , "Manager" , 50000,8543804393],
                 [2,"Pooaj" , "Salesman" , 20000,9956400943],
@@ -161,7 +160,7 @@
     
             ];
             echo "<table border ='2px' cellpadding='4px' cellspacing = '0'>";
-                    echo "<h2>Table For php.</h2>";
+                
                 echo "<tr>
                         <th>Emp Id.</th>
                         <th>Emp Name.</th>
@@ -178,4 +177,158 @@
                 echo "</tr>";
             }
             echo "</table>";
-?>
+
+          
+                // Multidimensional Associative Array
+
+                
+              echo   "<h3> Multidimensinaol Associative Array</h3>";
+                $marks = [
+
+                    "Mukesh" => [
+                        "physics" => 78,
+                        "math"  => 85,
+                        "hindi" => 75
+                    ],
+                    "Rakesh" => [
+                        "physics" => 78,
+                        "math"  => 85,
+                        "hindi" => 75
+                    ],
+                    "Vikash" => [
+                        "physics" => 78,
+                        "math"  => 85,
+                        "hindi" => 75
+                    ],
+
+                ];
+                    echo "<table border ='2px' cellpadding='5px' cellspacing = 0'>";
+                    echo "<tr>
+                        <th>Student Name</th>
+                        <th>Hindi</th>
+                        <th>Math</th>
+                        <th>English</th>
+                    </tr>";
+            foreach($marks as $key => $v1){
+                echo "<tr><td>$key</td>";
+            foreach( $v1 as $key => $v2){
+                echo "<td>  $v2</td>";
+
+            }
+            echo "</tr>";
+            }
+        echo "</table>";
+                               
+        
+                    // PHP Array : Count $ Sizeof()
+                    echo   "<h3> PHP Array : Count $ Sizeof()</h3>";
+
+            $food = [
+                "Apple","Banana","Graps","Mango"
+            ];
+            echo count($food);
+            echo "<ul>";
+            foreach($food as $value){
+                echo   "<li> $value </li>";
+
+            }
+            echo "</ul>";
+
+
+
+                    // Array Raplace function 
+
+                    echo "<h3> Array Raplace function.. </h3>";
+
+                $fruit = ['orange','mango','graps','Apple'];
+
+                $veggie = ['carrote','pea'];
+
+            $newArray = array_replace($fruit,$veggie);
+            
+            echo "<pre>";
+            print_r($newArray);
+            echo "</pre>";
+
+
+
+
+        // PHP Array_marge & Array_Combine.
+
+            echo "<h3>PHP Array_marge & Array_Combine..</h3>";
+
+        $fruit = ['a'=>"orange",
+        'b'=>"mango",
+        'c'=>"graps",
+        'd'=>"Apple"];
+
+        $veggie = ['carrote','pea'];
+
+        $colors = ['Red','Yellow'];
+
+        $newArray = array_merge($fruit,$veggie, $colors);
+
+        echo "<pre>";
+        print_r($newArray);
+        echo "</pre>";
+
+                // Array Splise function
+                echo "<h3>Array Splise</h3>";
+
+                $fruit = ['orange','mango','graps','Apple'];
+
+                $veggie = ['carrote','pea'];
+            
+                array_splice($fruit ,2,0,$veggie);
+
+                echo "<pre>";
+                print_r($fruit);
+                echo "</pre>";
+
+
+
+            // Array Key Function()
+
+
+        
+            echo "<h3> Array Key Function...</h3>";
+         
+        $name = [
+            "Name" => "Mukesh Kumar",
+            "Class" => 12,
+            "Address" => "Katarupur Post Patti",
+            "Mo"  => 8543804393,
+            "Email Id" => "My6386887@gmail.com"
+        ];
+        //    $newArray =  array_keys($name);
+        //    $newArray =  array_key_first($name);
+           $newArray =  array_key_last($name);
+
+           echo "<pre>";
+           print_r($newArray);
+           echo "</pre>";
+
+
+
+                        // Array Value & Keys Function
+                        
+                        
+        echo "<h3>Array Value & Keys Function</h3>";
+
+        $name = [
+            "Name" => "Mukesh Kumar",
+            "Class" => 12,
+            "Address" => "Katarupur Post Patti",
+            "Mo"  => 8543804393,
+            "Mo" => 8543804393,
+            "Email Id" => "My6386887@gmail.com"
+        ];
+            $newArray = array_values($name);
+            $newArray = array_unique($name);
+
+                echo "<pre>";
+                print_r($newArray);
+                echo "</pre>";
+            
+          
+ ?>
